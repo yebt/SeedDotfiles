2a9df44d1b9f28d96d3a165c08ba0fbcc69003aa
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-19 10:03:22 -0500

    add colors module for reuse

---
+++ modules/colors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+################################
+## Colors definition
+## @yebt
+##
+################################
+
+FG_YLLW='\e[38;2;231;195;14m'	# Yellow
+FG_RNG='\e[38;2;247;99;43m'	# Orange
+FG_RD='\e[38;2;251;76;71m'	# Red
+FG_PRPL='\e[38;2;155;139;255m'	# Purple
+FG_BL='\e[38;2;40;122;224m'	# Blue
+FG_GRN='\e[38;2;73;192;101m'	# Verde
+FG_MGNT='\e[38;2;250;109;190m'	# Magenta
+FG_CYN='\e[38;2;142;225;215m'	# Cian
+
+BG_YLLW='\e[48;2;231;195;14m'	# Bg Yellow
+BG_RNG='\e[48;2;247;99;43m'	# Bg Orange
+BG_RD='\e[48;2;251;76;71m'	# Bg Red
+BG_PRPL='\e[48;2;155;139;255m'	# Bg Purple
+BG_BL='\e[48;2;40;122;224m'	# Bg Blue
+BG_GRN='\e[48;2;73;192;101m'	# Bg Verde
+BG_MGNT='\e[48;2;250;109;190m'	# Bg Magenta
+BG_CYN='\e[48;2;142;225;215m'	# Bg Cian
+
+RST='\e[0m'
+
